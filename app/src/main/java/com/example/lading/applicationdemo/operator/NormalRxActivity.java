cd6949f4f172ac876f18b8982c33a9a95ab908fa
peiniwan <410093000@qq.com>
2017-07-31 17:57:05 +0800

    RxLifecycle

--- app/src/main/java/com/example/lading/applicationdemo/operator/NormalRxActivity.java
+++ app/src/main/java/com/example/lading/applicationdemo/operator/NormalRxActivity.java
@@ -1,12 +1,13 @@
 package com.example.lading.applicationdemo.operator;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.lading.applicationdemo.R;
+import com.trello.rxlifecycle.android.ActivityEvent;
+import com.trello.rxlifecycle.components.RxActivity;
 
 import rx.Observable;
 import rx.Subscriber;
@@ -14,23 +15,40 @@ import rx.Subscriber;
 /*
     同步情况下了解Rxjava的运行
  */
-public class NormalRxActivity extends AppCompatActivity implements View.OnClickListener{
+public class NormalRxActivity extends RxActivity implements View.OnClickListener {
 
     private TextView mText;
     private Button mBtn;
     private TextView mEdit;
-    static String str ="一二三四五\n 上山打老虎\n 老虎一发威\n 武松就发怵\n";
+    static String str = "一二三四五\n 上山打老虎\n 老虎一发威\n 武松就发怵\n";
+    private TextView mText2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout1);
         initView();
+        //模拟内存泄露
+        testRxlife();
     }
 
+    private void testRxlife() {
+//        myObservable
+//                .compose(bindUntilEvent(ActivityEvent.DESTROY))   //使用bindUntilEvent指定在哪个生命周期方法调用时取消订阅
+//                .subscribe();
+//        myObservable
+//                .compose(bindToLifecycle())  //自动取消对Observable订阅
+//                .subscribe();
+    }
+
+
     private void initView() {
-        mText= (TextView) findViewById(R.id.text1);
-        mEdit= (TextView) findViewById(R.id.edit1);
-        mBtn= (Button) findViewById(R.id.button);
+        mText = (TextView) findViewById(R.id.text1);
+        mText2 = (TextView) findViewById(R.id.text2);
+
+
+        mEdit = (TextView) findViewById(R.id.edit1);
+        mBtn = (Button) findViewById(R.id.button);
         mEdit.setText(str);
 
 
@@ -42,13 +60,13 @@ public class NormalRxActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.text1:
                 break;
             case R.id.edit1:
                 break;
             case R.id.button:
-                if(mText.getText().toString()!=null ||mText.getText().toString().length()>0){
+                if (mText.getText().toString() != null || mText.getText().toString().length() > 0) {
                     mText.setText("");
                 }
                 start();
@@ -58,13 +76,14 @@ public class NormalRxActivity extends AppCompatActivity implements View.OnClickL
 
     private void start() {
         //创建被观察者
-        Observable observable=createObservable();
+        Observable observable = createObservable();
         //创建观察者
-        Subscriber subscriber=createSubscriber();
+        Subscriber subscriber = createSubscriber();
 
         mText.append("开始订阅，准备观察...\n");
         //事实上，observable不止可以订阅subscriber，也可以订阅ActionX()
-        observable.subscribe(subscriber);
+        observable.compose(this.bindUntilEvent(ActivityEvent.PAUSE))
+                .subscribe(subscriber);
 
         //就像现在这样
 //        observable.subscribe(new Action1<String>() {
@@ -80,7 +99,7 @@ public class NormalRxActivity extends AppCompatActivity implements View.OnClickL
 
     private Subscriber createSubscriber() {
         //创建观察者
-        Subscriber subscriber=new Subscriber<String>() {
+        Subscriber subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
                 mText.append("执行观察者中的onCompleted()...\n");
@@ -91,19 +110,20 @@ public class NormalRxActivity extends AppCompatActivity implements View.OnClickL
             public void onError(Throwable e) {
 
             }
+
             @Override
             public void onNext(String s) {
                 mText.append("执行观察者中的onNext()...\n");
-                mText.append(s+"...\n");
+                mText.append(s + "...\n");
             }
 
         };
-        return  subscriber;
+        return subscriber;
     }
 
-    private Observable createObservable(){
+    private Observable createObservable() {
         //创建被观察者，这是最正常的创建方法
-        Observable observable=Observable.create(new Observable.OnSubscribe<String>(){
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
 
             @Override
             public void call(Subscriber<? super String> subscriber) {
